8ce0da2e49552e8fbc14ae2c5fd64d957473dc08
Elena Laptop <21sylver2003@gmail.com>
2026-05-17 20:04:46 +0300

    Card changes

--- frontend/src/components/pages/ProductPageComponents/RatingAndReviewsPage.jsx
+++ frontend/src/components/pages/ProductPageComponents/RatingAndReviewsPage.jsx
@@ -17,8 +17,9 @@ const RatingAndReviewsPage = () => {
         </div>
 
         {/*Reviews*/}
-        <div className="grid grid-cols-1 lg:grid-cols-2">
-            <ReviewCard/>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+            <ReviewCard username={"Samantha D."} reviewText={`I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt.`} reviewStar={4.5}/>
+            <ReviewCard username={"Ethan.R"} reviewStar={3.5} reviewText={"This t-shirt is a must-have for anyone who appreciates good design. The minimalistic yet stylish pattern caught my eye, and the fit is perfect. I can see the designer's touch in every aspect of this shirt."}/>
         </div>
 
     </div>
--- frontend/src/components/pages/ProductPageComponents/ReviewCard.jsx
+++ frontend/src/components/pages/ProductPageComponents/ReviewCard.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import StarRating from '../../OtherComponents/ProductComponents/StarRating'
 import { FaCheckCircle } from "react-icons/fa"
-const ReviewCard = () => {
+import { BsThreeDots } from "react-icons/bs";
+
+const ReviewCard = ({username, reviewText, reviewStar}) => {
   return (
     <div className="flex flex-col border border-black/10 rounded-[20px] px-7 py-7 gap-4">
         <div className="flex items-center gap-2 md:gap-4 justify-between">
-             <StarRating rating={4.5} starClassName="product_star_rate" divClassName="product_star_divClass"/>
-            <h4 className="font-satoshi text-[12px] md:text-[15px]">...</h4>
+             <StarRating rating={reviewStar} starClassName="product_star_rate" divClassName="product_star_divClass"/>
+             <BsThreeDots size={24} className="text-gray-600"/>
         </div>
 
         <div className="flex flex-col gap-2">
-            <h1 className="font-satoshibold text-[16px] lg:text-[20px] flex items-center gap-2">Samantha D.<FaCheckCircle className="text-green-600" /></h1>
+            <h1 className="font-satoshibold text-[16px] lg:text-[20px] flex items-center gap-2">{username}<FaCheckCircle className="text-green-600" /></h1>
 
-        <p className="font-satoshi flex flex-wrap text-black/50 text-[14px] lg:text-[16px]">"I absolutely love this t-shirt! The design is unique and the fabric feels so comfortable. As a fellow designer, I appreciate the attention to detail. It's become my favorite go-to shirt."</p>
+        <p className="font-satoshi flex flex-wrap text-black/50 text-[14px] lg:text-[16px]">{reviewText}</p>
 
         </div>
-        <span>Posted on August 14, 2023</span>
+        <span className="font-satoshi text-[14px] text-[16px] opacity-60">Posted on August 14, 2023</span>
     </div>
   )
 }
